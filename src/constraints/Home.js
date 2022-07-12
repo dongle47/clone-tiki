7252ca7bf568271bf3152fe0fbd051d0ae2b5ed4
19110339 <19110339@student.hcmute.edu.vn>
2022-07-12 15:34:52 +0700

    Tạo cái slider và card flashsale

---
+++ src/constraints/Home.js
@@ -0,0 +1,399 @@
+
+export const Products = [{
+    id: "1",
+    name: "Sữa Tắm Dưỡng Thể Từ Thiên Nhiên Dove Căng Bóng Thư Giãn Với Chiết Xuất Lavender Và Hương Thảo 530G",
+    image: "https://salt.tikicdn.com/cache/200x200/ts/product/5e/ab/4e/6ab0ba2c73e2ab0e48ee4cd2fed28630.jpg.webp",
+    price: "113.000",
+    sale: true,
+    percent_sale: "-21%",
+    rate: 4,
+  },
+  {
+    id: "2",
+    name: "Sữa Tắm Dưỡng Thể Từ Thiên Nhiên Dove Căng Bóng Thư Giãn Với Chiết Xuất Lavender Và Hương Thảo 530G",
+    image: "https://salt.tikicdn.com/cache/200x200/ts/product/5e/ab/4e/6ab0ba2c73e2ab0e48ee4cd2fed28630.jpg.webp",
+    price: "113.000",
+    sale: false,
+    percent_sale: "-21%",
+    rate: 3,
+  },
+  {
+    id: "3",
+    name: "Sữa Tắm Dưỡng Thể Từ Thiên Nhiên Dove Căng Bóng Thư Giãn Với Chiết Xuất Lavender Và Hương Thảo 530G",
+    image: "https://salt.tikicdn.com/cache/200x200/ts/product/5e/ab/4e/6ab0ba2c73e2ab0e48ee4cd2fed28630.jpg.webp",
+    price: "113.000",
+    sale: true,
+    percent_sale: "-21%",
+    rate: 5,
+  }
+    ,
+  {
+    id: "4",
+    name: "Sữa Tắm Dưỡng Thể Từ Thiên Nhiên Dove Căng Bóng Thư Giãn Với Chiết Xuất Lavender Và Hương Thảo 530G",
+    image: "https://salt.tikicdn.com/cache/200x200/ts/product/5e/ab/4e/6ab0ba2c73e2ab0e48ee4cd2fed28630.jpg.webp",
+    price: "113.000",
+    sale: true,
+    percent_sale: "-21%",
+    rate: 4,
+  },
+  {
+    id: "5",
+    name: "Sữa Tắm Dưỡng Thể Từ Thiên Nhiên Dove Căng Bóng Thư Giãn Với Chiết Xuất Lavender Và Hương Thảo 530G",
+    image: "https://salt.tikicdn.com/cache/200x200/ts/product/5e/ab/4e/6ab0ba2c73e2ab0e48ee4cd2fed28630.jpg.webp",
+    price: "113.000",
+    sale: true,
+    percent_sale: "-21%",
+    rate: 2,
+  },
+  {
+    id: "6",
+    name: "Sữa Tắm Dưỡng Thể Từ Thiên Nhiên Dove Căng Bóng Thư Giãn Với Chiết Xuất Lavender Và Hương Thảo 530G",
+    image: "https://salt.tikicdn.com/cache/200x200/ts/product/5e/ab/4e/6ab0ba2c73e2ab0e48ee4cd2fed28630.jpg.webp",
+    price: "113.000",
+    sale: false,
+    percent_sale: "-21%",
+    rate: 2,
+  },
+  {
+    id: "7",
+    name: "Sữa Tắm Dưỡng Thể Từ Thiên Nhiên Dove Căng Bóng Thư Giãn Với Chiết Xuất Lavender Và Hương Thảo 530G",
+    image: "https://salt.tikicdn.com/cache/200x200/ts/product/5e/ab/4e/6ab0ba2c73e2ab0e48ee4cd2fed28630.jpg.webp",
+    price: "113.000",
+    sale: true,
+    percent_sale: "-21%",
+    rate: 1,
+  }]
+  
+  export const Categories = [{
+    id:1,
+    display:"Thịt, rau củ",
+    link:""
+  },
+  {
+    id:2,
+    display:"Bách hoá",
+    link:""
+  },
+  {
+    id:3,
+    display:"Nhà cửa",
+    link:""
+  },
+  {
+    id:4,
+    display:"Điện tử",
+    link:""
+  },
+  {
+    id:5,
+    display:"Thiết bị số",
+    link:""
+  },
+  {
+    id:6,
+    display:"Điện Thoại",
+    link:""
+  },
+  {
+    id:7,
+    display:"Mẹ & Bé",
+    link:""
+  },
+  {
+    id:8,
+    display:"Làm Đẹp",
+    link:""
+  },
+  {
+    id:9,
+    display:"Gia Dụng",
+    link:""
+  },
+  {
+    id:10,
+    display:"Thời trang nữ",
+    link:""
+  },
+  {
+    id:11,
+    display:"Thời trang nam",
+    link:""
+  },
+  {
+    id:12,
+    display:"Giày nữ",
+    link:""
+  },
+  {
+    id:13,
+    display:"Túi nữ",
+    link:""
+  },
+  {
+    id:14,
+    display:"Giày nam",
+    link:""
+  },
+  {
+    id:15,
+    display:"Túi nam",
+    link:""
+  },
+  {
+    id:16,
+    display:"Balo & Vali",
+    link:""
+  },
+  {
+    id:17,
+    display:"Phụ kiện",
+    link:""
+  },
+  {
+    id:18,
+    display:"Đồng hồ",
+    link:""
+  },
+  {
+    id:19,
+    display:"Laptop",
+    link:""
+  },
+  {
+    id:20,
+    display:"Quốc Tế",
+    link:""
+  },
+  {
+    id:21,
+    display:"Voucher",
+    link:""
+  },
+  {
+    id:22,
+    display:"Xe",
+    link:""
+  },
+  {
+    id:23,
+    display:"Sách",
+    link:""
+  },
+  {
+    id:24,
+    display:"Thể Thao",
+    link:""
+  },
+  {
+    id:25,
+    display:"Máy Ảnh",
+    link:""
+  }
+  ]
+
+export const SlideKhuyenMai1=[
+    {
+        id:1,
+        image:"https://salt.tikicdn.com/cache/w1080/ts/banner/e1/24/ea/8d16a304c1de6b1e9a12586cb489765f.png.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:2,
+        image:"https://salt.tikicdn.com/cache/w1080/ts/banner/54/dc/2d/411f8f572f51b9fbf1653e0c3ee6f5d6.png.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:3,
+        image:"https://salt.tikicdn.com/cache/w1080/ts/banner/4e/ec/28/71aa878fcf6a4d5ae2b2579ac6c0ba42.png.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:4,
+        image:"https://salt.tikicdn.com/cache/w1080/ts/banner/ff/3d/e6/29bf0ceee4ae3c239a4c8895291ebf3d.png.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:5,
+        image:"https://salt.tikicdn.com/cache/w1080/ts/banner/90/ed/cc/4282895d0552372653e8e753a9e47819.png.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:6,
+        image:"https://salt.tikicdn.com/cache/w1080/ts/banner/d0/9b/ff/dd99bd105dff7cd7dcff6b72926f11b7.png.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:7,
+        image:"https://salt.tikicdn.com/cache/w1080/ts/banner/06/03/99/71526328c1f55f8343e65d47e4dec24b.png.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:8,
+        image:"https://salt.tikicdn.com/cache/w1080/ts/banner/a1/b1/c6/17a7b69dd325f7d06e1d663ccb7a8cab.png.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:9,
+        image:"https://salt.tikicdn.com/cache/w1080/ts/banner/07/be/16/fc5457a1f72bbbb8fff3d82bfc682c9f.jpg.webp",
+        alt:"",
+        link:""
+    }
+]
+
+export const SlideThuongHieu1=[
+    {
+        id:1,
+        image:"https://salt.tikicdn.com/cache/w750/ts/banner/6a/0c/75/46be5701ee9693e04313497bd884f7d2.png.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:2,
+        image:"https://salt.tikicdn.com/cache/w750/ts/banner/dc/4b/95/34bca244e96f33baa49431c61281c6ac.jpg.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:3,
+        image:"https://salt.tikicdn.com/cache/w750/ts/banner/cb/96/f2/08f5f94f4abc298ee9b5d59764ad9282.jpg.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:4,
+        image:"https://salt.tikicdn.com/cache/w750/ts/banner/12/63/8a/4f95a8501a917f37ab2285b404cd514f.jpg.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:5,
+        image:"https://salt.tikicdn.com/cache/w750/ts/banner/90/c1/5f/2b68fd0f97a967c8d35d3096c5f37f05.jpg.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:6,
+        image:"https://salt.tikicdn.com/cache/w750/ts/banner/b9/ff/d5/613029482ba19136e2bdce2f276e0a9e.png.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:7,
+        image:"https://salt.tikicdn.com/cache/w750/ts/banner/1f/7a/55/13dfeebc9782d8ac15d8446f62b5db0a.jpg.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:8,
+        image:"https://salt.tikicdn.com/cache/w750/ts/banner/f7/fb/89/d48dd8c1b62ed11d2e51211eb1230454.png.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:9,
+        image:"https://salt.tikicdn.com/cache/w750/ts/banner/d9/32/17/d10ef505722de7f8d0a1d2a4d21b2f60.png.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:10,
+        image:"https://salt.tikicdn.com/cache/w750/ts/banner/6a/0c/75/46be5701ee9693e04313497bd884f7d2.png.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:11,
+        image:"https://salt.tikicdn.com/cache/w750/ts/banner/6d/7c/26/030e3341616afcc6f4d4cd2717bf9db4.jpg.webp",
+        alt:"",
+        link:""
+    },
+    {
+        id:12,
+        image:"https://salt.tikicdn.com/cache/w750/ts/banner/5d/01/8c/0db5f642ccc888717d875be46051908e.png.webp",
+        alt:"",
+        link:""
+    }
+]
+  
+export const SlideThuongHieu2=[
+    {
+        id:1,
+        image:"https://salt.tikicdn.com/cache/w200/ts/banner/83/e9/29/d97a2b3854aefbcf8a902edd41ef7f49.png.webp",
+        alt:"Chính Hãng Giảm 90%",
+        link:""
+    },
+    {
+        id:2,
+        image:"https://salt.tikicdn.com/cache/w200/ts/banner/c1/fa/5e/deda5b0b31f4c9f05c901e8e3ed7813a.jpg.webp",
+        alt:"Săn Coupon 300K",
+        link:""
+    },
+    {
+        id:3,
+        image:"https://salt.tikicdn.com/cache/w200/ts/banner/43/d7/b5/70fe3c89f6c4aef1c7f74118afd2ce57.jpg.webp",
+        alt:"Săn Coupon 300K",
+        link:""
+    },
+    {
+        id:4,
+        image:"https://salt.tikicdn.com/cache/w200/ts/banner/3c/c1/c0/829bc27d5a0dd7026033c899c1d2ad1e.png.webp",
+        alt:"Coupon 50K",
+        link:""
+    },
+    {
+        id:5,
+        image:"https://salt.tikicdn.com/cache/w200/ts/banner/69/80/1d/339ae8880fb3ca5e9b1675c3d2f2bbfd.jpg.webp",
+        alt:"Săn Coupon 360K",
+        link:""
+    },
+    {
+        id:6,
+        image:"https://salt.tikicdn.com/cache/w200/ts/banner/1f/78/45/0404f603ca275362f9973dff812521fd.png.webp",
+        alt:"Săn Coupon 200K",
+        link:""
+    },
+    {
+        id:7,
+        image:"https://salt.tikicdn.com/cache/w200/ts/banner/fe/d9/ce/080b8eae59733eb3295a4108caff3b7a.png.webp",
+        alt:"Mua 2 Giảm 15%",
+        link:""
+    },
+    {
+        id:8,
+        image:"https://salt.tikicdn.com/cache/w200/ts/banner/c1/52/3f/a07a0af107709a24df5fda4a4fc65eb5.png.webp",
+        alt:"Mua 1 Tặng 1",
+        link:""
+    },
+    {
+        id:9,
+        image:"https://salt.tikicdn.com/cache/w200/ts/banner/72/1a/a0/3e1be3e1c7408b9b9f248f475177553c.png.webp",
+        alt:"Sách Dưới 40K",
+        link:""
+    },
+    {
+        id:10,
+        image:"https://salt.tikicdn.com/cache/w200/ts/banner/16/8a/ef/20982817373ab8ce90f347d612dd37c7.png.webp",
+        alt:"Chỉ Từ 99K",
+        link:""
+    },
+    {
+        id:11,
+        image:"https://salt.tikicdn.com/cache/w200/ts/banner/94/f2/30/636853948c627d78c902c0b50dea11a9.png.webp",
+        alt:"Giảm Đến 50%",
+        link:""
+    },
+    {
+        id:12,
+        image:"https://salt.tikicdn.com/cache/w200/ts/banner/68/37/2f/a0d50fdac60d0f438efdb81e5dd929c4.png.webp",
+        alt:"Mua 1 Tặng 1",
+        link:""
+    }
+]
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,86 +3,163 @@ import "./Home.scss"
 import { Link } from "react-router-dom"
 import Grid from '@mui/material/Grid';
 import CardProduct from '../../components/CardProduct';
-import Container from '@mui/material/Container';
+import CardFlashsale from '../../components/CardFlashsale';
+import { Products, Categories, SlideKhuyenMai1, SlideThuongHieu1, SlideThuongHieu2 } from '../../constraints/Home';
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import required modules
+import { Pagination, Navigation } from "swiper";
+
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 function Home() {
   return (
     <>
-      <div>Trang chủ</div><br />
-      <div><Link to={"/tim-kiem"} className=""> Đến trang Tìm kiếm</Link></div>
-      <br></br>
-      <div className='container' style={{ maxWidth: "1270px" }}>
+      <div style={{ marginTop: "2rem",backgroundColor:"#f5f5fa" }}>
+        <div className='category'>
+        <div className='container'>
+          <Swiper
+            slidesPerView={13}
+            slidesPerGroup={13}
+            navigation={true}
+            modules={[Navigation]}
+            className="mySwiper"
+            
+          >
+            {
+              Categories.map(item =>
+                <SwiperSlide key={item.id} style={{ margin: "auto" }}>
+                  <div style={{ fontSize: "14px", textAlign: "center" }}>{item.display}</div>
+                </SwiperSlide>)
+            }
+          </Swiper>
+          </div>
+        </div>
+        <div className='container'>
+
+          <section id="section1">
+            <div style={{ width: "66.65%" }}>
+              <Swiper
+                navigation={true}
+                loop={true}
+                pagination={{
+                  clickable: true,
+                }}
+                modules={[Navigation, Pagination]}
+                className="mySwiper"
+                id="slider-khuyenmai"
+              >
+                {SlideKhuyenMai1.map(item =>
+                  <SwiperSlide key={item.id}>
+                    <div style={{ width: "100%" }}>
+                      <img style={{ width: "100%" }} src={item.image} alt=""/>
+                    </div>
+                  </SwiperSlide>)}
+              </Swiper>
+            </div>
+            <div style={{ width: "32.7%" }}>
+              <img style={{ width: "100%" }} alt="" src="https://salt.tikicdn.com/cache/w400/ts/banner/a7/ee/b9/b4931904b38e1c5d9236345aa8b02a67.png.webp" />
+            </div>
+          </section>
+
+          <section id="section2">
+            <div style={{ width: "59.35%",height:"274px",backgroundColor:"#fff",borderRadius:"4px" }}>
+              <div id="section2__heading">
+                <div id="section2__title">
+                  <img alt="" src="https://frontend.tikicdn.com/_desktop-next/static/img/giasoc.svg"  />
+                  <img alt="" src="https://frontend.tikicdn.com/_desktop-next/static/img/dealFlashIcon.svg"/>
+                  <img src="https://frontend.tikicdn.com/_desktop-next/static/img/homnay.svg" alt="" />
+                </div>
+                <Link to={"/xemthem"}>Xem thêm</Link>
+              </div>
+              <Swiper
+              slidesPerView={5}
+              slidesPerGroup={5}
+                navigation={true}
+                modules={[Navigation]}
+                className="mySwiper"
+                id="slider-thuonghieu2"
+              >
+                {Products.map(item =>
+                  <SwiperSlide style={{minWidth:"148px"}} key={item.id}>
+                    <CardFlashsale data={item}/>
+                  </SwiperSlide>)}
+              </Swiper>
+            </div>
+            <div style={{ width: "39.35%" }}>
+              <img style={{ width: "100%" }} src="https://salt.tikicdn.com/cache/w572/ts/upload/1a/8e/00/291b67286660dbc51bb8381633868927.png.webp" />
+            </div>
+          </section>
+
+          <section id="section3"></section>
+          <section id="section4"></section>
+          <section id="section5"></section>
+          <section id="section6">
+            <div className="section__heading">
+              <div className="section__title">
+                <span><img style={{ width: "24px" }} src="https://salt.tikicdn.com/ts/upload/33/0f/67/de89fab36546a63a8f3a8b7d038bff81.png" alt="" /></span>
+                Thương Hiệu Chính Hãng
+              </div>
+              <Link to={"/xem-them"}>XEM THÊM</Link>
+            </div>
+
+            <Swiper
+              slidesPerView={2}
+              spaceBetween={30}
+              slidesPerGroup={2}
+              navigation={true}
+              loop={true}
+              pagination={{
+                clickable: true,
+              }}
+              modules={[Navigation, Pagination]}
+              className="mySwiper"
+              id="slider-thuonghieu1"
+            >
+              {SlideThuongHieu1.map(item =>
+                <SwiperSlide key={item.id} >
+                  <div style={{ width: "100%" }}>
+                    <img style={{ width: "100%", borderRadius: "8px" }} src={item.image} />
+                  </div>
+                </SwiperSlide>)}
+            </Swiper>
+
+            <Swiper
+              slidesPerView={6}
+              slidesPerGroup={6}
+              navigation={true}
+              modules={[Navigation]}
+              className="mySwiper"
+              id="slider-thuonghieu2"
+            >
+              {SlideThuongHieu2.map(item =>
+                <SwiperSlide key={item.id} style={{ padding: "0 8px" }} >
+                  <div className='img__thuonghieu2' style={{ width: "100%" }}>
+                    <img style={{ width: "100%", borderRadius: "8px" }} src={item.image} />
+                    <p>{item.alt}</p>
+                  </div>
+                </SwiperSlide>)}
+            </Swiper>
+          </section>
+
+
+
+        </div>
+
+
+      </div>
+      <div className='container'>
         <Grid container >
-            {Products.map(item=> <Grid key={item.id}  item lg={2} md={4} sm={6} xs={6}><CardProduct data={item} /></Grid> )}
-          </Grid>
+          {Products.map(item => <Grid key={item.id} item lg={2} md={4} sm={6} xs={6}><CardProduct data={item} /></Grid>)}
+        </Grid>
       </div>
     </>
 
   )
 }
 
-const Products = [{
-  id:"1",
-  name:"Sữa Tắm Dưỡng Thể Từ Thiên Nhiên Dove Căng Bóng Thư Giãn Với Chiết Xuất Lavender Và Hương Thảo 530G",
-  image:"https://salt.tikicdn.com/cache/200x200/ts/product/5e/ab/4e/6ab0ba2c73e2ab0e48ee4cd2fed28630.jpg.webp",
-  price:"113.000",
-  sale:true,
-  percent_sale:"-21%",
-  rate:4,
-},
-{
-  id:"2",
-  name:"Sữa Tắm Dưỡng Thể Từ Thiên Nhiên Dove Căng Bóng Thư Giãn Với Chiết Xuất Lavender Và Hương Thảo 530G",
-  image:"https://salt.tikicdn.com/cache/200x200/ts/product/5e/ab/4e/6ab0ba2c73e2ab0e48ee4cd2fed28630.jpg.webp",
-  price:"113.000",
-  sale:false,
-  percent_sale:"-21%",
-  rate:3,
-},
-{
-  id:"3",
-  name:"Sữa Tắm Dưỡng Thể Từ Thiên Nhiên Dove Căng Bóng Thư Giãn Với Chiết Xuất Lavender Và Hương Thảo 530G",
-  image:"https://salt.tikicdn.com/cache/200x200/ts/product/5e/ab/4e/6ab0ba2c73e2ab0e48ee4cd2fed28630.jpg.webp",
-  price:"113.000",
-  sale:true,
-  percent_sale:"-21%",
-  rate:5,
-}
-,
-{
-  id:"4",
-  name:"Sữa Tắm Dưỡng Thể Từ Thiên Nhiên Dove Căng Bóng Thư Giãn Với Chiết Xuất Lavender Và Hương Thảo 530G",
-  image:"https://salt.tikicdn.com/cache/200x200/ts/product/5e/ab/4e/6ab0ba2c73e2ab0e48ee4cd2fed28630.jpg.webp",
-  price:"113.000",
-  sale:true,
-  percent_sale:"-21%",
-  rate:4,
-},
-{
-  id:"5",
-  name:"Sữa Tắm Dưỡng Thể Từ Thiên Nhiên Dove Căng Bóng Thư Giãn Với Chiết Xuất Lavender Và Hương Thảo 530G",
-  image:"https://salt.tikicdn.com/cache/200x200/ts/product/5e/ab/4e/6ab0ba2c73e2ab0e48ee4cd2fed28630.jpg.webp",
-  price:"113.000",
-  sale:true,
-  percent_sale:"-21%",
-  rate:2,
-},
-{
-  id:"6",
-  name:"Sữa Tắm Dưỡng Thể Từ Thiên Nhiên Dove Căng Bóng Thư Giãn Với Chiết Xuất Lavender Và Hương Thảo 530G",
-  image:"https://salt.tikicdn.com/cache/200x200/ts/product/5e/ab/4e/6ab0ba2c73e2ab0e48ee4cd2fed28630.jpg.webp",
-  price:"113.000",
-  sale:false,
-  percent_sale:"-21%",
-  rate:2,
-},
-{
-  id:"7",
-  name:"Sữa Tắm Dưỡng Thể Từ Thiên Nhiên Dove Căng Bóng Thư Giãn Với Chiết Xuất Lavender Và Hương Thảo 530G",
-  image:"https://salt.tikicdn.com/cache/200x200/ts/product/5e/ab/4e/6ab0ba2c73e2ab0e48ee4cd2fed28630.jpg.webp",
-  price:"113.000",
-  sale:true,
-  percent_sale:"-21%",
-  rate:1,
-}]
 
 export default Home
