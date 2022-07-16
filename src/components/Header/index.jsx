7f44ca558b04a1e617b17223e063f87d501e359e
19110382 <19110382@student.hcmute.edu.vn>
2022-07-16 13:30:30 +0700

    login khương

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Header.scss";
+import { useState, useCallback } from "react"
 
 import {
   Grid,
@@ -11,6 +12,7 @@ import {
   Badge,
   Box,
   Modal,
+  TextField,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 
@@ -22,10 +24,17 @@ import StorefrontOutlinedIcon from "@mui/icons-material/StorefrontOutlined";
 import CloseIcon from "@mui/icons-material/Close";
 
 function Header() {
-  const [modalLogin, setModalLogin] = React.useState(false);
+  const [modalLogin, setModalLogin] = useState(false);
+  const [loginForm, setLoginForm] = useState(true);
   const openModalLogin = () => setModalLogin(true);
-  const closeModalLogin = () => setModalLogin(false);
+  const closeModalLogin = () => {
+    setModalLogin(false);
+    setLoginForm(true)
+  }
 
+  const handleLogin = useCallback(() => {
+    setLoginForm(false)
+  })
   return (
     <Stack
       justifyContent="space-between"
@@ -125,19 +134,85 @@ function Header() {
       </Stack>
 
       <Modal open={modalLogin} onClose={closeModalLogin}>
-        <Box className="modal-login" >
-          <Stack direction="row" justifyContent="space-between">
-            <Typography variant="h6" component="h2">
-              Login
-            </Typography>
-            <IconButton onClick={closeModalLogin}>
-              <CloseIcon />
-            </IconButton>
-          </Stack>
-        </Box>
+        <Box className="modal-login" sx={{ width: "800px" }}>
+          {
+            loginForm ? <Login handleLogin={handleLogin} closeModalLogin={closeModalLogin} /> : <Signin closeModalLogin={closeModalLogin} />
+          }</Box>
       </Modal>
     </Stack>
   );
+}
+
+function Login(props) {
+  return (
+    <Stack direction="row">
+      <Stack direction='column' sx={{ flex: 5 }} spacing={2}>
+        <h4 style={{ fontSize: "24px" }}>Xin chào,</h4>
+        <p style={{ fontSize: "15px" }}>Đăng nhập hoặc tạo tài khoản</p>
+        <TextField id="standard-basic" label="Số Điện Thoại" variant="standard" />
+        <Button variant="contained" onClick={props.handleLogin} color='error' >Tiếp tục</Button>
+        <p style={{ textAlign: "center" }}>Đăng nhập bằng mail</p>
+        <p style={{ textAlign: "center", marginTop: "3rem" }} >Tiếp tục bằng</p>
+        <Stack direction="row" justifyContent="center" alignItems="center" spacing={2} >
+          <img src="https://salt.tikicdn.com/ts/upload/3a/22/45/0f04dc6e4ed55fa62dcb305fd337db6c.png" alt="facebook" width="58px" height="58px" />
+          <img src="https://salt.tikicdn.com/ts/upload/1c/ac/e8/141c68302262747f5988df2aae7eb161.png" alt="google" width="58px" height="58px" />
+        </Stack>
+        <p style={{ textAlign: "center" }}>Bằng việc tiếp tục, bạn đã chấp nhận <a href="">điều khoản sử dụng</a></p>
+      </Stack>
+      <Box sx={{ flex: 3, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: "10px" }}>
+        <img src="https://salt.tikicdn.com/ts/upload/eb/f3/a3/25b2ccba8f33a5157f161b6a50f64a60.png" width="203" />
+        <h4>Mua sắm tại Tiki</h4>
+        <span>Siêu ưu đãi mỗi ngày</span>
+      </Box>
+      <span style={{ position: "absolute", top: 0, right: 0 }}>
+        <IconButton onClick={props.closeModalLogin}>
+          <CloseIcon />
+        </IconButton>
+      </span>
+
+    </Stack>
+  )
+}
+
+function Signin(props) {
+  return (
+    <Stack direction="row">
+      <Stack direction='column' sx={{ flex: 5 }} spacing={2}>
+        <h4 style={{ fontSize: "24px" }}>Đăng ký</h4>
+        <p style={{ fontSize: "15px" }}>tạo tài khoản</p>
+        <TextField id="standard-basic" label="Nhập họ tên" variant="standard" />
+        <div style={{width:"100%", display:"flex"}}>
+          <TextField id="standard-basic" label="Số điện thoại" variant="standard" sx={{flex:1}} />
+          <Button variant="outlined" sx={{width:"100px"}}>Gửi mã</Button>
+        </div>
+        <div style={{width:"100%", display:"flex"}}>
+          <TextField id="standard-basic" label="Nhập mã" variant="standard" sx={{flex:1}}/>
+          <Button variant="outlined" sx={{width:"100px"}}>Xác nhận</Button>
+        </div>
+        <TextField id="standard-basic" label="Nhập mật khẩu" variant="standard" />
+        <TextField id="standard-basic" label="Nhập lại mật khẩu" variant="standard" />
+        <Button variant="contained" color='error'>Hoàn Tất</Button>
+        <p style={{ textAlign: "center" }}>Đăng nhập bằng mail</p>
+        <p style={{ textAlign: "center", marginTop: "3rem" }} >Tiếp tục bằng</p>
+        <Stack direction="row" justifyContent="center" alignItems="center" spacing={2} >
+          <img src="https://salt.tikicdn.com/ts/upload/3a/22/45/0f04dc6e4ed55fa62dcb305fd337db6c.png" alt="facebook" width="58px" height="58px" />
+          <img src="https://salt.tikicdn.com/ts/upload/1c/ac/e8/141c68302262747f5988df2aae7eb161.png" alt="google" width="58px" height="58px" />
+        </Stack>
+        <p style={{ textAlign: "center" }}>Bằng việc tiếp tục, bạn đã chấp nhận <a href="">điều khoản sử dụng</a></p>
+      </Stack>
+      <Box sx={{ flex: 3, display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", gap: "10px" }}>
+        <img src="https://salt.tikicdn.com/ts/upload/eb/f3/a3/25b2ccba8f33a5157f161b6a50f64a60.png" width="203" />
+        <h4>Mua sắm tại Tiki</h4>
+        <span>Siêu ưu đãi mỗi ngày</span>
+      </Box>
+      <span style={{ position: "absolute", top: 0, right: 0 }}>
+        <IconButton onClick={props.closeModalLogin}>
+          <CloseIcon />
+        </IconButton>
+      </span>
+
+    </Stack>
+  )
 }
 
 export default Header;
