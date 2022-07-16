b584e6c343dc71936848b1f59b56db952c310ede
Dong Le <61934131+dongle47@users.noreply.github.com>
2022-07-16 22:21:15 +0700

    complete account info

--- src/pages/CustomerAccount/Info/Password/index.jsx
+++ src/pages/CustomerAccount/Info/Password/index.jsx
@@ -1,9 +1,81 @@
+import {
+  Box,
+  Stack,
+  InputBase,
+  Typography,
+  Button,
+  TextField,
+  InputAdornment,
+  IconButton,
+} from "@mui/material";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
 
+import { useState } from "react";
 
-function Password(){
+function Password() {
+  const [showPass, setShowPass] = useState(false);
+  const [showNewPass, setShowNewPass] = useState(false);
+  const [showConfirmPass, setShowConfirmPass] = useState(false);
+
+  const passwordInput = (placeHolder) => {
     return (
-        <div>Đổi mật khẩu</div>
-    )
+      <TextField
+        size="small"
+        label={placeHolder}
+        type={showPass ? "text" : "password"}
+        name="pass"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton onClick={() => setShowPass(!showPass)}>
+                {showPass ? (
+                  <VisibilityOutlinedIcon />
+                ) : (
+                  <VisibilityOffOutlinedIcon />
+                )}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+    );
+  };
+
+  return (
+    <Stack sx={{ mt: "5rem" }} spacing={1} >
+      <Typography variant="h5">Đổi mật khẩu</Typography>
+
+      <Stack
+        sx={{ width: "60rem", height: "25rem", backgroundColor: "white" }}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Stack
+          sx={{
+            border: "0.5px solid #e0dfde",
+            borderRadius: "4px",
+            padding: "1rem",
+            width: "40%",
+          }}
+          spacing={3}
+        >
+          {passwordInput("Nhập mật khẩu hiện tại")}
+
+          <Stack>
+            {passwordInput("Nhập mật khẩu mới")}
+            <Typography variant="caption">
+              Mật khẩu phải dài từ 8 đến 32 ký tự, bao gồm chữ và số
+            </Typography>
+          </Stack>
+
+          {passwordInput("Nhập lại mật khẩu hiện mới")}
+
+          <Button variant="contained">Lưu thay đổi</Button>
+        </Stack>
+      </Stack>
+    </Stack>
+  );
 }
 
-export default Password
+export default Password;
--- src/pages/CustomerAccount/Info/PhoneNumber/index.jsx
+++ src/pages/CustomerAccount/Info/PhoneNumber/index.jsx
@@ -1,9 +1,45 @@
+import { Box, Stack, InputBase, Typography, Button } from "@mui/material";
+import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
+import { borderRadius } from "@mui/system";
 
+function PhoneNumber() {
+  return (
+    <Box sx={{ mt: "5rem" }}>
+      <Typography>Cập nhật số điện thoại</Typography>
+      
+      <Stack
+        sx={{ width: "60rem", height: "15rem"  }}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Stack
+          sx={{
+            border: "0.5px solid #e0dfde",
+            borderRadius: "4px",
+            padding: "1rem",
+            width: "40%",
+            
+          }}
+        >
+          <Typography>Số điện thoại</Typography>
 
-function PhoneNumber(){
-    return (
-        <div>Cập nhật số điện thoại</div>
-    )
+          <Stack spacing={4}>
+            <Stack
+              sx={{ border: "1px solid darkgrey", borderRadius: "4px" }}
+              direction="row"
+              alignItems="center"
+              spacing={1}
+            >
+              <LocalPhoneOutlinedIcon sx={{ ml: "6px" }} color="disabled" />
+              <InputBase placeholder="Nhập số điện thoại" />
+            </Stack>
+
+            <Button variant="contained">Lưu thay đổi</Button>
+          </Stack>
+        </Stack>
+      </Stack>
+    </Box>
+  );
 }
 
-export default PhoneNumber
+export default PhoneNumber;
