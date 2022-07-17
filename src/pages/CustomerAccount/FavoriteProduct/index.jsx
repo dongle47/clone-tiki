fdbef56886b907a43e681b6c466bc3f68fedfa39
Dong Le <61934131+dongle47@users.noreply.github.com>
2022-07-17 16:08:23 +0700

    add favorite product page

--- src/pages/CustomerAccount/FavoriteProduct/index.jsx
+++ src/pages/CustomerAccount/FavoriteProduct/index.jsx
@@ -1,9 +1,39 @@
+import {
+  Box,
+  Stack,
+  Typography,
+  Card,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Button,
+} from "@mui/material";
+
+import CardProduct from "../../../components/CardProduct";
+import productImage from "../../../assets/img/avatar1.jpg";
 
 
-function FavoriteProduct(){
-    return(
-        <div>Danh sách yêu thích</div>
-    )
+function FavoriteProduct() {
+  return (
+    <Box>
+      <Typography>Danh sách yêu thích</Typography>
+      <Stack>
+        <Card sx={{ border: "0px solid black", maxWidth: "13rem" }}>
+          <CardMedia component="img" image={productImage} height="200" />
+
+          <CardContent sx={{ padding: "5px 0 0 0" }}>
+            <Typography variant="caption" color="text.secondary">
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit...
+            </Typography>
+          </CardContent>
+
+          <CardActions>
+          
+          </CardActions>
+        </Card>
+      </Stack>
+    </Box>
+  );
 }
 
-export default FavoriteProduct
+export default FavoriteProduct;
