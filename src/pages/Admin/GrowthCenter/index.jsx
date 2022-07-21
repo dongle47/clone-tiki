4ba2e3730717562832f427468de91db539fd1406
simpCocoGoat <duyduongdg@gmail.com>
2022-07-21 10:06:49 +0700

    cài thư viện x-date-picker, x-date-picker-pro và date-fns để làm datePicker

--- src/pages/Admin/GrowthCenter/index.jsx
+++ src/pages/Admin/GrowthCenter/index.jsx
@@ -1,6 +1,46 @@
-function GrowthCenter(){
+import * as React from "react";
+import "./GrowthCenter.scss";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { LocalizationProvider } from '@mui/x-date-pickers-pro';
+import { AdapterDateFns } from '@mui/x-date-pickers-pro/AdapterDateFns';
+import { DateRangePicker } from '@mui/x-date-pickers-pro/DateRangePicker';
+
+function GrowthCenter() {
+    const [value, setValue] = React.useState([null, null]);
     return(
-        <div>Trung tâm phát triển</div>
+        <Box>
+            <Stack>
+                <Typography>Hiệu quả kinh doanh</Typography>
+                <Stack direction="row">
+                    <Typography>Thời gian báo cáo:</Typography>
+                    <Button variant="outlined">Hôm nay</Button>
+                    <Button variant="outlined">7 ngày qua</Button>
+                    <Button variant="outlined">30 ngày qua</Button>
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DateRangePicker
+                            value={value}
+                            onChange={(newValue) => {
+                            setValue(newValue);
+                            }}
+                            renderInput={(startProps, endProps) => (
+                            <React.Fragment>
+                                <TextField {...startProps} />
+                                <Box sx={{ mx: 2 }}> to </Box>
+                                <TextField {...endProps} />
+                            </React.Fragment>
+                            )}
+                        />
+                    </LocalizationProvider>
+                    <Typography>(Lần cập nhật cuối: 18/06/2022. 11:00)</Typography>
+                </Stack>
+            </Stack>
+            
+        </Box>
+
     )
 }
 
