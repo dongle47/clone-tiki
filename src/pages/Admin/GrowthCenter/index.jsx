dbb08d6b82ec846b937ca7d4c52789b5fabeb296
simpCocoGoat <duyduongdg@gmail.com>
2022-07-22 22:07:30 +0700

    chỉnh sửa giao diện phát triển

--- src/pages/Admin/GrowthCenter/index.jsx
+++ src/pages/Admin/GrowthCenter/index.jsx
@@ -11,13 +11,83 @@ import { DateRangePicker } from "@mui/x-date-pickers-pro/DateRangePicker";
 import ArrowDropUpIcon from "@mui/icons-material/ArrowDropUp";
 import DownloadIcon from "@mui/icons-material/Download";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+export const options = {
+  responsive: true,
+  interaction: {
+    mode: "index",
+    intersect: false,
+  },
+  stacked: false,
+  plugins: {
+    title: {
+      display: true,
+      text: "Chart.js Line Chart - Multi Axis",
+    },
+  },
+  scales: {
+    y: {
+      type: "linear",
+      display: true,
+      position: "left",
+    },
+    y1: {
+      type: "linear",
+      display: true,
+      position: "right",
+      grid: {
+        drawOnChartArea: false,
+      },
+    },
+  },
+};
+const labels = ["January", "February", "March", "April", "May", "June", "July"];
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [35, 65, 95, 35, 67, 70, 40],
+      borderColor: "rgb(255, 99, 132)",
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+      yAxisID: "y",
+    },
+    {
+      label: "Dataset 2",
+      data: [350, 450, 750, 650, 470, 769, 570],
+      borderColor: "rgb(53, 162, 235)",
+      backgroundColor: "rgba(53, 162, 235, 0.5)",
+      yAxisID: "y1",
+    },
+  ],
+};
 
 function GrowthCenter() {
   const [value, setValue] = React.useState([null, null]);
   return (
     <Box padding="1rem">
       <Stack spacing={2}>
-        <Stack backgroundColor="#ffff" pl="1rem" pt="1rem">
+        <Stack backgroundColor="#ffff" pl="1rem" pt="1rem" pb="1rem" spacing={1.5}>
           <Typography
             sx={{ fontSize: "20px", fontWeight: "500", color: "#000000d9" }}
           >
@@ -72,14 +142,15 @@ function GrowthCenter() {
                   setValue(newValue);
                 }}
                 renderInput={(startProps, endProps) => (
-                  <React.Fragment maxHeight="32px" justifyContent="center">
-                    <TextField {...startProps} />
+                  <React.Fragment justifyContent="center">
+                    <TextField size="small" {...startProps} />
                     <Box sx={{ mx: 1 }}> to </Box>
-                    <TextField {...endProps} />
+                    <TextField size="small" {...endProps} />
                   </React.Fragment>
                 )}
               />
             </LocalizationProvider>
+
             <Typography
               sx={{ fontSize: "14px", fontWeight: "", color: "#000000d9" }}
             >
@@ -89,13 +160,17 @@ function GrowthCenter() {
         </Stack>
         <Stack spacing={2} backgroundColor="#ffff" pl="1rem" pt="1rem">
           {/*  */}
-          <Stack direction="row" sx={{ alignItems: "center" }} spacing={1}>
+          <Stack
+            direction="row"
+            sx={{ alignItems: "center", justifyContent: "space-between" }}
+            spacing={1}
+          >
             <Typography
               sx={{ fontSize: "14px", fontWeight: "600", color: "#000000d9" }}
             >
               Chỉ số chính
             </Typography>
-            <Typography sx={{ fontSize: "14px", color: "#00000073" }}>
+            <Typography sx={{ fontSize: "14px", color: "#00000073", flex: 1 }}>
               7 ngày qua: 14/07/2022 - 20/07/2022 (So sánh với: 05/07/2022 -
               11/07/2022)
             </Typography>
@@ -105,7 +180,7 @@ function GrowthCenter() {
           </Stack>
 
           <Stack direction="row" spacing={3}>
-            <Stack className="Box-info">
+            <Stack className="Box-info" >
               <Stack
                 direction="row"
                 sx={{ display: "flex", justifyContent: "space-between" }}
@@ -352,6 +427,15 @@ function GrowthCenter() {
                 </Typography>
               </Stack>
             </Stack>
+          </Stack>
+        </Stack>
+        <Stack
+          backgroundColor="#ffff"
+          direction="row"
+          sx={{ justifyContent: "center" }}
+        >
+          <Stack sx={{ width: "70%", justifyContent: "center" }}>
+            <Line options={options} data={data} />
           </Stack>
         </Stack>
       </Stack>
