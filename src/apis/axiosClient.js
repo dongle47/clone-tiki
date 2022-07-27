d23f76665d38cc6cde7953cc15fc86f44e08c00d
19110339 <19110339@student.hcmute.edu.vn>
2022-07-27 09:25:39 +0700

    Tạo API getProduct

--- src/apis/axiosClient.js
+++ src/apis/axiosClient.js
@@ -1,13 +1,10 @@
-
 import axios from 'axios';
 import queryString from 'query-string';
 import jwt_decode from 'jwt-decode';
-import getData from './getData';
-const baseURL='https://bebaomatweb.herokuapp.com/api'
+const baseURL='https://playerhostedapitest.herokuapp.com/api/'
 //const baseURL='http://localhost:5000/api'
 export const axiosClient = axios.create({
     baseURL: baseURL,
-    
     headers: {
         "Content-Type": "application/json"
     },
@@ -19,7 +16,6 @@ export const axiosClient = axios.create({
 
 const refreshToken = async (user) => {
     const res = await axiosClient.post('/auth/refreshtoken', { refreshToken: user.refreshToken  }, { headers: { Authorization: `Bearer ${user.accessToken}` }, })
-    console.log(res)
     return res.data
 }
 
@@ -36,8 +32,7 @@ export const axiosInstance = (user, dispatch, stateSuccess) => {
             let date = new Date();
             const decodeToken = jwt_decode(user?.accessToken);
             if (decodeToken.exp < date.getTime() / 1000) {
-                const newAccessToken = getData(await refreshToken(user));
-                console.log(newAccessToken.accessToken)
+                const newAccessToken = await refreshToken(user);
                 const newUser = {
                     ...user,
                     accessToken: newAccessToken.accessToken
