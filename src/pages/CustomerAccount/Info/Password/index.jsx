9edc82bd67340ac8b73ffaedc276880919658185
simpCocoGoat <duyduongdg@gmail.com>
2022-07-31 17:54:18 +0700

    validation khi thay đổi mật khẩu

--- src/pages/CustomerAccount/Info/Password/index.jsx
+++ src/pages/CustomerAccount/Info/Password/index.jsx
@@ -14,38 +14,73 @@ import apiProfile from "../../../../apis/apiProfile";
 
 function Password() {
   const [showPass, setShowPass] = React.useState(false);
-  const [message, setMessage] = React.useState("");
-
   const [confirmPassword, setconfirmPassword] = React.useState("");
   const [newPassword, setnewPassword] = React.useState("");
   const [oldPassword, setoldPassword] = React.useState("");
-
+  const [message, setMessage] = React.useState("*");
+  const [newMessage, setNewMessage] = React.useState("*")
+  const [fontSizeMessage, setFontSizeMessage] = React.useState("")
+  //const [valid, setValid] = React.useState({ new: false, cf: false });
+  
+  const Rcolor = "#2196f3"
+  const Fcolor = "#ee0033"
+  const [color, setColor] = React.useState("");
+  const [newcolor, setNColor] = React.useState("");
+  
+  const strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.{8,})");//regex kiểm tra mật khẩu hợp lệ
+  
+  const onChangeoldPassword = (event) => {
+      setoldPassword(event.target.value)
+  }
+  
   const onChangenewPassword = (event) => {
     setnewPassword(event.target.value)
-    
+    if (strongRegex.test(event.target.value)) {
+      setColor(Rcolor)
+      setNewMessage("*Mật khẩu hợp lệ")
+      //setValid(pre => { return { ...pre, new: true } })
+    }
+    else {
+      setColor(Fcolor)
+      setNewMessage("*Mật khẩu phải có ít nhất 8 kí tự. Chứa kí tự thường, kí tự hoa và số")
+      //setValid(pre => { return { ...pre, new: false } })
+    }
   }
-  const onChangeoldPassword = (event) => {
-    setoldPassword(event.target.value)
-    
-  }
+  
   const onChangeconfirmPassword = (event) => {
     setconfirmPassword(event.target.value)
-    
+    if (newPassword.localeCompare(event.target.value) === 0) {
+      setFontSizeMessage("13px")
+      setNColor(Rcolor)
+      setMessage("*Trùng khớp")
+      //setValid(pre => { return { ...pre, cf: true } })
+    }
+    else {
+      setFontSizeMessage("14px")
+      setNColor(Fcolor)
+      setMessage("*Mật khẩu không trùng khớp!")
+      //setValid(pre => { return { ...pre, cf: false } })
+    }
   }
   const handleChangePassword = () => {
-    const params = {
-      "confirmPassword": confirmPassword,
-      "newPassword": newPassword,
-      "oldPassword": oldPassword
-    }
-    apiProfile.putChangePassword(params)
-      .then(response => {
-        setMessage("Thay đổi thành công")
+    //if (valid.new && valid.cf){
+      const params = {
+        "confirmPassword": confirmPassword,
+        "newPassword": newPassword,
+        "oldPassword": oldPassword
+      }
+      apiProfile.putChangePassword(params)
+        .then(response => {
+          setFontSizeMessage("16px")
+          setNColor(Rcolor)
+          setMessage("Thay đổi thành công")
+        })
+        .catch(error => {
+          setFontSizeMessage("16px")
+          setNColor(Fcolor)
+        setMessage("Thay đổi không thành công!")
       })
-      .catch(error => {
-      console.log(error)
-      setMessage("Thay đổi không thành công!")
-    })
+    //}   
   }
 
   const passwordInput = (placeHolder, value, onChange) => {
@@ -88,15 +123,18 @@ function Password() {
 
           <Stack>
             {passwordInput("Nhập mật khẩu mới", newPassword, onChangenewPassword)}
-            <Typography variant="caption">
-              
-            </Typography>
+            <Box height="40px"><Typography fontSize="13px" color={color}>
+              {newMessage}
+            </Typography></Box>
+            
           </Stack>
 
           {passwordInput("Nhập lại mật khẩu mới", confirmPassword, onChangeconfirmPassword)}
-          <Box><Typography>
+          <Box height="25px">
+            <Typography fontSize={fontSizeMessage} color={newcolor}>
               {message}
-            </Typography></Box>
+            </Typography>
+          </Box>
           
           <Button onClick={handleChangePassword} variant="contained">Lưu thay đổi</Button>
         </Stack>
